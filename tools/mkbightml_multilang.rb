73b25df332f263b6f68d1093d67bdc0c9fa5222d
Max <max.devel.projects@gmail.com>
2008-12-10 01:46:48 -0200

    Created the /mdowns/en directory. Moved *.mdown in /mdowns/en. Refactored /tools/mkbightml_multilang.rb to create a /html/@lang directory if needed and use the right directory. Changed /tools/head.rb to load the right *.js and *.css files and corrected nedia=screen.

---
+++ tools/mkbightml_multilang.rb
@@ -0,0 +1,24 @@
+# mkbightml.rb
+require 'rubygems'
+require 'bluecloth'
+
+ARGV[0]  ? @lang = ARGV[0] : @lang = 'en'
+
+puts "#@lang"
+
+data_directory = File.expand_path(File.join("../html/", "#@lang"))
+unless File.exist?(data_directory)
+      Dir.mkdir(data_directory)
+end
+
+big_mdown = IO.read('../README.mdown') + "\n\n"
+head = IO.read('./head.erb')
+
+Dir.glob("../mdowns/#@lang/*.mdown").each{|file| big_mdown << IO.read(file) << "\n\n"}
+
+b = BlueCloth.new big_mdown
+
+template = "<html><head>#{head}</head><body><div id='content'>#{b.to_html.gsub(/<code>/, '<code class="ruby">')}<div></body></html>"
+open("../html/#@lang/ShoesTutorialNote.html", 'w'){|f| f.puts template}
+=begin
+=end
